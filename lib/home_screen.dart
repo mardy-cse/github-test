e1bf4a2b110199bdb882bc45b8499a0d97bbee0c
John Liton Mardy <mardy.cse@gmail.com>
2024-03-11 20:26:01 +0600

    jksdf

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -6,7 +6,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('HomeScreen'),
+        title: Text('Secon Screen'),
       ),
     );
   }
